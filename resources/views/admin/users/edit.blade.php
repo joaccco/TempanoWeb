6394636ce581762fac0cc8d78141c70648c01a63
joaccco <82912317+joaccco@users.noreply.github.com>
2022-10-25 20:13:49 -0300

    Admin LTE (Create Rol User)

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -7,10 +7,20 @@
 @stop
 
 @section('content')
+
+  @if (session('info'))
+    <div class="alert alert-success">
+      <strong> {{session('info')}}</strong>
+    </div>
+  @endif
+
+
+
   <div class="card">
   <div class="card-body">
   <p class="h5">Nombre</p>
-  <p class="form-control">{{$user->name}}</p>  
+  <p class="form-control">{{$user->name}}</p>
+  <h2 class="h5">Lista de Roles</h2>  
       {!! Form::model($user, ['route' => ['admin.users.update', $user], 'method' => 'put' ]) !!}
       @foreach ($roles as $role)
          <div>
@@ -20,6 +30,7 @@
             </label>
          </div>
       @endforeach
+      {!! Form::submit('Asignar rol', ['class' => 'btn btn-primary mt-2']) !!}
       {!! Form::close() !!}
   </div>    
 

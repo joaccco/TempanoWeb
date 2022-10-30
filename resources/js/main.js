eb757abd0da99bc32d835fde1615cee8b077d8b7
Joaquin Franco <leandrojoaquinf2@gmail.com>
2022-10-29 22:29:14 -0300

    Changes (Map)

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,43 +5,20 @@ const map = new mapboxgl.Map({
     center: [-58.827, -27.460], // starting position [lng, lat]
     zoom: 12 // starting zoom
 });
-map.on('load', () => {
-    // Load an image from an external URL.
-    map.loadImage(
-    '/img/logo.png',
-    (error, image) => {
-    if (error) throw error;
-     
-    // Add the image to the map style.
-    map.addImage('cat', image);
-     
-    // Add a data source containing one point feature.
-    map.addSource('point', {
-    'type': 'geojson',
-    'data': {
-    'type': 'FeatureCollection',
-    'features': [
-    {
-    'type': 'Feature',
-    'geometry': {
-    'type': 'Point',
-    'coordinates': [-58.827, -27.46]
-    }
-    }
-    ]
-    }
+
+map.on('click', (event) => {
+    const features = map.queryRenderedFeatures(event.point, {
+    layers: ['chicago-parks']
     });
+    if (!features.length) {
+    return;
+    }
+    const feature = features[0];
      
-    // Add a layer to use the image to represent the data.
-    map.addLayer({
-    'id': 'points',
-    'type': 'symbol',
-    'source': 'point', // reference the data source
-    'layout': {
-    'icon-image': 'cat', // reference the image
-    'icon-size': 0.15
-    }
-    });
-    }
-    );
-    });
+    const popup = new mapboxgl.Popup({ offset: [0, -15] })
+    .setLngLat(feature.geometry.coordinates)
+    .setHTML(
+    `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+    )
+    .addTo(map);
+});
